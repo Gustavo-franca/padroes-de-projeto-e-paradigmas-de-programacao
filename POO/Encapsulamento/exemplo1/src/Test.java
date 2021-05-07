39ad0b16be055d50bdf38172c0a6b46864773570
Gustavo <gustavocandidofranca@gmail.com>
2021-05-07 09:27:15 -0300

    feat: adiciona exemplos e descriçao de encapsulamento

---
+++ POO/Encapsulamento/exemplo1/src/Test.java
@@ -0,0 +1,35 @@
+package Encapsulamento.exemplo1.src;
+
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        CarDriver carDriver = new CarDriver();
+        Car car = new Car();
+
+        /* the driver opens  the car */ 
+        car.open();
+        /* the driver gets in the car */
+        car.enter(carDriver);
+        /* the driver closes the door*/
+        car.close();
+        // the driver starts the car
+        car.turnOn();
+        // the driver  accelerates the car
+        car.speedUp();
+        // the driver brekes the car 
+        car.brake();
+        // the driver shuts down the car
+        car.turnOff();
+        // the driver gets out of the car
+        car.exit();
+        // the driver closes the car
+        car.close();
+        
+
+    
+
+    }
+    
+}
---
+++ POO/Encapsulamento/exemplo2/v1/People.java
@@ -0,0 +1,50 @@
+package Encapsulamento.exemplo2.v1;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class People {
+     
+    private String name;
+    private int age;
+    private List<String> phones;
+    private People mother;
+    
+    public People(String name, int age,People mother){
+        this.name = name;
+        this.age = age;
+        this.phones = new ArrayList<>();
+        this.mother = mother;
+    }
+
+    public People getMother() {
+        return mother;
+    }
+
+    public String name(){
+        return this.name;
+    }
+    public void name(String name){
+        this.name = name;
+    }
+
+    public void age(int age){
+        this.age = age;
+    }
+    public int age(){
+        return this.age;
+    }
+    public void addPhone(String phone){
+       if(phone.matches("([0-9]+)")){
+           this.phones.add(phone);
+           return;
+       };
+       throw new Error("invalid phone number format!");
+    }
+
+    public List<String> getPhones(){
+        return this.phones;
+    }
+}
+
